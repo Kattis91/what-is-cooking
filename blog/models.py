92bf7939374c9fbf3c6c075aad8b7aaea15aa6d8
Kattis91 <katya.svedmark@gmail.com>
2023-09-28 20:17:39 +0000

    Add Recipe model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_lengt=100, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='published_recipes')
+    featured_image = CloudinaryField('image', default='placeholder')
+    instructions = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    estimated_time = models.IntegerField('Estimated Time')
+    likes = models.ManyToManyField(User, related_name='recipe_likes', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'{self.title} | {self.author}'
+
+
+
