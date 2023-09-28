3bb70b66e86f187ba58a9a7f12d90d4958c839b1
Kattis91 <katya.svedmark@gmail.com>
2023-09-28 21:56:19 +0000

    Create Comment model

--- blog/models.py
+++ blog/models.py
@@ -21,4 +21,19 @@ class Recipe(models.Model):
         return f'{self.title} | {self.author}'
 
 
+class Comment(models.Model):
+
+    post = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=50, unique=True)
+    email = models.EmailField()
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f'Comment by {self.name}'
 
