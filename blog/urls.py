304fb4d1d7ea6dfc9d159b79d2a7daba4072247c
Kattis91 <katya.svedmark@gmail.com>
2023-10-01 19:22:55 +0000

    Create the vie codew and URL pattern t be able too view categoy list

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("recipes/", views.RecipeList.as_view(), name="recipes"),
+    path("", views.CategoryList.as_view(), name="categories")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Recipe
+from .models import Recipe, Category
 
 
 def check_the_base(request):
@@ -12,3 +12,10 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.order_by("-created_on")
     template_name = "recipes.html"
     pagination = 8
+
+
+class CategoryList(generic.ListView):
+    model = Category
+    queryset = Category.objects.all()
+    template_name = "index.html"
+
