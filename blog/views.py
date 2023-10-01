feb0c18aba5a3b24bf0ee20847a56bee3407357e
Kattis91 <katya.svedmark@gmail.com>
2023-10-01 16:24:39 +0000

    Create the view code for the recipes page

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,11 @@ from .models import Recipe
 
 
 def check_the_base(request):
-    return render(request, 'base.html')
+    return render(request, 'index.html')
 
+
+class RecipeList(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.order_by("-created_on")
+    template_name = "recipes.html"
+    pagination = 8
