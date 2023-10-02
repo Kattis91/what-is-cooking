95d001ab2b9d6b2802d4e0d9d306404059e15719
Kattis91 <katya.svedmark@gmail.com>
2023-10-02 22:26:16 +0000

    Add the view code and URL path to be able to diplay recipe_detail.html

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("recipes/", views.RecipeList.as_view(), name="recipes"),
-    path("", views.CategoryList.as_view(), name="categories")
+    path("", views.CategoryList.as_view(), name="categories"),
+    path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe, Category
 
 
@@ -12,6 +12,26 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.order_by("-created_on")
     template_name = "recipes.html"
     pagination = 8
+
+
+class RecipeDetail(View):
+    def get(self, request, slug, *args, **kvargs):
+        queryset = Recipe.objects
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            'recipe_detail.html',
+            {
+                'recipe': recipe,
+                'comments': comments,
+                'liked': liked,
+            }
+        )
 
 
 class CategoryList(generic.ListView):
