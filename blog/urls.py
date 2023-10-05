9dc65bca3c4bb2f2c02c783cbcc245a63958682c
Kattis91 <katya.svedmark@gmail.com>
2023-10-05 11:09:46 +0000

    Create an URL pattern for add_recipe page

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path("recipes/", views.RecipeList.as_view(), name="recipes"),
     path("", views.CategoryList.as_view(), name="categories"),
+    path('add_recipe/', views.AddRecipe.as_view(), name='add_recipe'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
 ]
