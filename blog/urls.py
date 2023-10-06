32da538054f432873fcc36527a21f72e28b2d8e5
Kattis91 <katya.svedmark@gmail.com>
2023-10-06 19:57:28 +0000

    Add 'edit' button visible only for recipe author

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.CategoryList.as_view(), name="categories"),
     path('add_recipe/', views.AddRecipe.as_view(), name='add_recipe'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
+    path('update/<slug:slug>', views.UpdateRecipeView.as_view(), name='update')
 ]
