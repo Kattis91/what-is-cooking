2d5c4c083bf6c87d8ea9d7c4f03081dbe9f2c353
Kattis91 <katya.svedmark@gmail.com>
2023-10-07 16:15:14 +0000

    Add a URL path for CategoryListView

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
     path('update/<slug:slug>', views.UpdateRecipeView.as_view(), name='update'),
     path('delete/<slug:slug>', views.DeleteRecipeView.as_view(), name='delete'),
+    path('category/<category>/', views.CategoryListView.as_view(), name='category'),
 ]
