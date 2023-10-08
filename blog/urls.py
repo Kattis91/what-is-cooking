5e166672f7108f0d1b8c172818994fee133fa170
Kattis91 <katya.svedmark@gmail.com>
2023-10-08 18:18:45 +0000

    Add a new URL path for likes

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('update/<slug:slug>', views.UpdateRecipeView.as_view(), name='update'),
     path('delete/<slug:slug>', views.DeleteRecipeView.as_view(), name='delete'),
     path('category/<category>/', views.CategoryListView.as_view(), name='category'),
+    path('like/<slug:slug>', views.RecipeLike.as_view(), name='recipe_like'),
 ]
