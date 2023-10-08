f76c77227ccfd9752bfb7245069fef74a2a25ad3
Kattis91 <katya.svedmark@gmail.com>
2023-10-08 15:56:05 +0000

    Add a post-method to enable users send comments

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Recipe, Category
-from .forms import RecipeForm
+from .forms import RecipeForm, CommentForm
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
@@ -36,7 +36,40 @@ class RecipeDetail(View):
             {
                 'recipe': recipe,
                 'comments': comments,
+                'commented': False,
                 'liked': liked,
+                'comment_form': CommentForm()
+            }
+        )
+
+    def post(self, request, slug, *args, **kvargs):
+        queryset = Recipe.objects
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.recipe = recipe
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            'recipe_detail.html',
+            {
+                'recipe': recipe,
+                'comments': comments,
+                'commented': True,
+                'liked': liked,
+                'comment_form': comment_form
             }
         )
 
