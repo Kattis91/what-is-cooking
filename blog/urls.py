45529fb5e8b274c8d194b8a3fcf7892c6b156387
Kattis91 <katya.svedmark@gmail.com>
2023-10-08 11:42:55 +0000

    Change the name of the path for the home page to 'home'

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("recipes/", views.RecipeList.as_view(), name="recipes"),
-    path("", views.CategoryList.as_view(), name="categories"),
+    path('', views.CategoryList.as_view(), name='home'),
+    path('recipes/', views.RecipeList.as_view(), name='recipes'),
     path('add_recipe/', views.AddRecipe.as_view(), name='add_recipe'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
     path('update/<slug:slug>', views.UpdateRecipeView.as_view(), name='update'),
