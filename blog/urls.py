539020ff0828b686dc9a9806cd99726222a91e45
Kattis91 <katya.svedmark@gmail.com>
2023-10-13 16:57:37 +0000

    Tidy up to pass through CI Python Linter

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,11 @@ urlpatterns = [
     path('recipes/', views.RecipeList.as_view(), name='recipes'),
     path('add_recipe/', views.AddRecipe.as_view(), name='add_recipe'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
-    path('update/<slug:slug>', views.UpdateRecipeView.as_view(), name='update'),
-    path('delete/<slug:slug>', views.DeleteRecipeView.as_view(), name='delete'),
-    path('category/<category>/', views.CategoryListView.as_view(), name='category'),
+    path(
+        'update/<slug:slug>', views.UpdateRecipeView.as_view(), name='update'),
+    path(
+        'delete/<slug:slug>', views.DeleteRecipeView.as_view(), name='delete'),
+    path('category/<category>/',
+         views.CategoryListView.as_view(), name='category'),
     path('like/<slug:slug>', views.RecipeLike.as_view(), name='recipe_like'),
 ]
