c4fdb6c3fc899e11e0b5ac379e85268f054e535b
Kattis91 <katya.svedmark@gmail.com>
2023-10-16 17:26:14 +0000

    Tidy up. Add comments

--- blog/views.py
+++ blog/views.py
@@ -10,12 +10,35 @@ from django.http import HttpResponseRedirect
 
 
 class CategoryList(generic.ListView):
+    """
+    Renders all objects of Category model
+    """
     model = Category
     queryset = Category.objects.all()
     template_name = "index.html"
 
 
+class CategoryListView(generic.ListView):
+    """
+    Allows to filter recipes by categories
+    """
+    template_name = "category.html"
+    context_object_name = "catlist"
+
+    # Source: https://www.youtube.com/watch?v=S9-Bt1JgRjQ&t=2137s
+    def get_queryset(self):
+        content = {
+            'cat': self.kwargs['category'],
+            'posts': Recipe.objects.filter(
+                category__name=self.kwargs['category'])
+        }
+        return content
+
+
 class RecipeList(generic.ListView):
+    """
+    Renders all objects of Recipe model as a list
+    """
     model = Recipe
     queryset = Recipe.objects.order_by("-created_on")
     template_name = "recipes.html"
@@ -23,11 +46,14 @@ class RecipeList(generic.ListView):
 
 
 class RecipeDetail(View):
+    """
+    Displays recipe details in the view.
+    Allows logged-in users to leave comments
+    """
     def get(self, request, slug, *args, **kvargs):
-        queryset = Recipe.objects
+        queryset = Recipe.objects.all()
         recipe = get_object_or_404(queryset, slug=slug)
         ingredients = recipe.ingredients.splitlines()
-        print(ingredients)
         comments = recipe.comments.filter(approved=True).order_by('created_on')
         liked = False
         if recipe.likes.filter(id=self.request.user.id).exists():
@@ -79,11 +105,15 @@ class RecipeDetail(View):
 
 
 class AddRecipe(CreateView):
+    """
+    Allows logged-in users to publish recipes
+    """
     model = Recipe
     form_class = RecipeForm
     template_name = 'add_recipe.html'
     success_url = reverse_lazy('recipes')
 
+    # Source: https://stackoverflow.com/questions/67366138/django-display-message-after-creating-a-post # noqa
     def form_valid(self, form):
         form.instance.author = self.request.user
         success_message = "Your recipe has been posted successfully."
@@ -92,6 +122,10 @@ class AddRecipe(CreateView):
 
 
 class UpdateRecipeView(UpdateView):
+    """
+    Allows recipe authors (when logged in)
+    to update their published recipes
+    """
     model = Recipe
     template_name = 'update_recipe.html'
     form_class = RecipeForm
@@ -105,11 +139,16 @@ class UpdateRecipeView(UpdateView):
 
 
 class DeleteRecipeView(SuccessMessageMixin, DeleteView):
+    """
+    Allows recipe authors (when logged in)
+    to delete their published recipes
+    """
     model = Recipe
     template_name = 'delete_recipe.html'
     form_class = RecipeForm
     success_url = reverse_lazy('recipes')
 
+    # Source: https://stackoverflow.com/questions/47636968/django-messages-for-a-successfully-delete-add-or-edit-item # noqa
     success_message = "Your recipe has been deleted successfully"
 
     def delete(self, request, *args, **kwargs):
@@ -117,20 +156,10 @@ class DeleteRecipeView(SuccessMessageMixin, DeleteView):
         return super(DeleteRecipeView, self).delete(request, *args, **kwargs)
 
 
-class CategoryListView(generic.ListView):
-    template_name = "category.html"
-    context_object_name = "catlist"
-
-    def get_queryset(self):
-        content = {
-            'cat': self.kwargs['category'],
-            'posts': Recipe.objects.filter(
-                category__name=self.kwargs['category'])
-        }
-        return content
-
-
 class RecipeLike(View):
+    """
+    Allows logged-in users to like/unlike recipes
+    """
     def post(self, request, slug, *args, **kwargs):
         recipe = get_object_or_404(Recipe, slug=slug)
 
@@ -139,3 +168,4 @@ class RecipeLike(View):
         else:
             recipe.likes.add(request.user)
         return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
+
